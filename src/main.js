0d8d010cd804e409675f86b21d4514b4b14c6caa
franckysolo <franckysolo@gmail.com>
2020-02-26 23:43:49 +0100

    example issue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import Ionic from '@ionic/vue'
+import "@ionic/core/css/core.css"
+import "@ionic/core/css/ionic.bundle.css"
+
+Vue.use(Ionic)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Ion Select Issue</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Testing ion-select selected values</h1>
+      <p>
+        Example from
+        <a href="https://ionicframework.com/docs/api/select#properties">
+          documentation
+        </a>
+      </p>
+      <ion-list class="ion-margin-top">
+        <ion-item>
+          <ion-label>Pets</ion-label>
+          <ion-select id="my-select" multiple="true" :value="['bird', 'dog']">
+            <ion-select-option value="bird">Bird</ion-select-option>
+            <ion-select-option value="cat">Cat</ion-select-option>
+            <ion-select-option value="dog">Dog</ion-select-option>
+            <ion-select-option value="honeybadger">Honey Badger</ion-select-option>
+          </ion-select>
+        </ion-item>
+      </ion-list>
+      <ion-button @click="forceCheckingValues">
+        Force Checking defaults
+      </ion-button>
+    </ion-content>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      defaultValues: ['cat', 'dog'] // not working when bind this value to ion-select
+    }
+  },
+  async mounted () {
+    // not working when setting defaultValues to ion-select on mounted
+    this.$set(this.$data, 'defaultValues', ['bird', 'honeybadger'])
+    // this is woring example
+    await this.forceCheckingValues()
+  },
+  methods: {
+    async forceCheckingValues () {
+      const select = await document.querySelector('#my-select')
+      if (select) {
+        window.console.info('select input is ready')
+      }
+      select.value = ['bird', 'dog']
+    }
+  }
+}
+</script>
